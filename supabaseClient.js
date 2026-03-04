57bb7e5d945a54e66d4e8f1d12a62af6fac9dc1f
Simon Hanot <simon.hanot@bhc.be>
2026-03-04 22:23:55 +0100

    Initial Project

--- supabaseClient.js
+++ supabaseClient.js
@@ -1,7 +1,4 @@
-import 'react-native-url-polyfill/auto';
+import { SUPABASE_URL, SUPABASE_ANON_KEY } from '@env';
 import { createClient } from '@supabase/supabase-js';
-
-const SUPABASE_URL = 'https://fvfpsyvduwimdwcrbvib.supabase.co';
-const SUPABASE_ANON_KEY = 'sb_publishable_EaNS_E-n9jAxOIudD8nqpA_3GwTnCpW';
 
 export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
